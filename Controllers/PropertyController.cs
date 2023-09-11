f2240954e231a0e2756a3aeae05a834188d3b3ca
Vunguye1 <s364540@oslomet.no>
2023-09-11 17:22:00 +0200

    temporary changes

--- Controllers/PropertyController.cs
+++ Controllers/PropertyController.cs
@@ -4,17 +4,32 @@ namespace Project1.Controllers
 {
     public class PropertyController : Controller
     {
-        public IActionResult General() // this view will return both leilighet and hus
+        public IActionResult GeneralGrid() // this view will return both leilighet and hus
         {
             return View();
         }
 
-        public IActionResult Apartment() // only apartments
+        public IActionResult GeneralTable() // general table view
         {
             return View();
         }
 
-        public IActionResult House() // only houses
+        public IActionResult ApartmentGrid() // only apartments, grid layout
+        {
+            return View();
+        }
+
+        public IActionResult ApartmentTable() // only apartments, table layout
+        {
+            return View();
+        }
+
+        public IActionResult HouseGrid() // only houses, grid layout
+        {
+            return View();
+        }
+
+        public IActionResult HouseTable() // only houses, table layout
         {
             return View();
         }
