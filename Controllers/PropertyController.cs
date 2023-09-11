36218896e886077043bfcf9121b621c15dfbd291
Vunguye1 <s364540@oslomet.no>
2023-09-11 17:11:26 +0200

    Add project files.

---
+++ Controllers/PropertyController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project1.Controllers
+{
+    public class PropertyController : Controller
+    {
+        public IActionResult General() // this view will return both leilighet and hus
+        {
+            return View();
+        }
+
+        public IActionResult Apartment()
+        {
+            return View();
+        }
+
+        public IActionResult House()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Property.cs
@@ -0,0 +1,13 @@
+﻿namespace Project1.Models
+{
+    public class Property
+    {
+        public int PropertyID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public string? Description { get; set; }
+        public string? imageurl { get; set; }
+        
+    }
+}
