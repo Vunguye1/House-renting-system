2d62e19e67f394e29e36ed268fb88bd2374bf98e
siljeflaglien <s364579@oslomet.no>
2023-09-29 15:03:50 +0200

    Made Profile

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace Project1.Controllers
         {
             return View();
         }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
     }
 }
