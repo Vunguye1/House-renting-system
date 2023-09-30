e047bce7bf5a8ac7f1d49309e02b825159bf59a2
Vunguye1 <s364540@oslomet.no>
2023-09-30 13:07:10 +0200

    add serilog, not yet added to Real estate controller

--- Program.cs
+++ Program.cs
@@ -1,22 +1,38 @@
 using Microsoft.EntityFrameworkCore;
 using Project1.Models;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<RealestateDbContext>(options => {
+// add NewtonsoftJson
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+
+builder.Services.AddDbContext<RealestateDbContext>(options =>
+{
     options.UseSqlite(
         builder.Configuration["ConnectionStrings:RealestateDbContextConnection"]);
 });
 
+// add serilog
+var loggerConfiguration = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.File($"Logs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log");
+
+loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
+                                                e.Level == Serilog.Events.LogEventLevel.Information &&
+                                                e.MessageTemplate.Text.Contains("Execute DbCommand"));
+
+var logger = loggerConfiguration.CreateLogger();
+builder.Logging.AddSerilog(logger);
+
+// build our app
 var app = builder.Build();
 
-/*if (!app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-*/
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
