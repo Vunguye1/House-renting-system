97feb556bd8b0c8625355a88d1935f073c9130cd
Vunguye1 <s364540@oslomet.no>
2023-10-10 21:00:05 +0200

    all executions on real estates are granted to admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Project1.Models;
+using Project1.ViewModels;
 
 namespace Project1.Controllers
 {
@@ -15,10 +16,74 @@ namespace Project1.Controllers
             _realestateDbContext = realestateDbContext;
         }
 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         public async Task<IActionResult> ListAllUsers() // List all users registered in database. Testing purpose
         {
             List<ApplicationUser> users = await _realestateDbContext.User.ToListAsync();
             return View(users);
+        }
+
+
+
+        public async Task<IActionResult> ListAllRealestates() // List all existing real estates
+        {
+            List<Realestate> propertylist = await _realestateDbContext.Realestates.ToListAsync();
+            var listmodel = new RealestateListViewModel(propertylist, "GeneralTable");
+            return View(listmodel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var item = await _realestateDbContext.Realestates.FindAsync(id);
+
+            if (item == null)
+            {
+                return BadRequest("item not found");
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Realestate realestate)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _realestateDbContext.Realestates.Update(realestate);
+                await _realestateDbContext.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(ListAllRealestates));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var realestate = await _realestateDbContext.Realestates.FindAsync(id);
+            if (realestate == null)
+            {
+                return NotFound();
+            }
+            return View(realestate);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var realestate = _realestateDbContext.Realestates.Find(id);
+            if (realestate == null)
+            {
+                return NotFound();
+            }
+            _realestateDbContext.Realestates.Remove(realestate);
+            await _realestateDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(ListAllRealestates));
         }
     }
 }
