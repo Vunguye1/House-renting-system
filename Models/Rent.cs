e0f1d2c4de20f04e2479d7b6d51d1868315f6930
Vunguye1 <s364540@oslomet.no>
2023-10-20 14:27:30 +0200

    fix delete user

--- Models/Rent.cs
+++ Models/Rent.cs
@@ -7,13 +7,13 @@ namespace Project1.Models
 		public string RentDateFrom { get; set; } = string.Empty;
 		public string RentDateTo { get; set; } = string.Empty;
 
-        public string UserId { get; set; } = string.Empty;
+        public string? UserId { get; set; } = string.Empty;
         //navigation property
-        public virtual ApplicationUser User { get; set; } = default!;
+        public virtual ApplicationUser? User { get; set; } = default!;
 
         public int RealestateId { get; set; }
         //navigation property
-        public virtual Realestate Realestate { get; set; } = default!;
+        public virtual Realestate? Realestate { get; set; } = default!;
 
 		public decimal TotalPrice { get; set; }
 		
