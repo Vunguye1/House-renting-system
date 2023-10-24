088b63afa415944205b9e080606dc474140ca22a
serinaerzengin <s364561@oslomet.no>
2023-10-24 13:40:20 +0200

    input validering serverside

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations; //to be able to do input validation
 
 
 namespace Project1.Models
@@ -9,8 +10,14 @@ namespace Project1.Models
          We want to add more details to user, and therefore we use this source as inspiration to our code:
         https://codewithmukesh.com/blog/user-management-in-aspnet-core-mvc/
          */
+
+        [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,}", ErrorMessage = "First name must be letters and be at least 2 characters long.")]
         public string FirstName { get; set; } = string.Empty;
+
+        [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,}", ErrorMessage = "Last name must be letters and be at least 2 characters long.")]
         public string LastName { get; set; } = string.Empty;
+
+        [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ. \-]{2,50}", ErrorMessage = "Address must consist of 2 to 50 letters or numbers.")]
         public string Adress { get; set; } = string.Empty;
 
         public byte[]? ProfilePicture { get; set; }
